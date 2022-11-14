0248f3791fb17227db591c0266327593a8004573
oseefotso <fotsoosee@gmail.com>
2022-11-14 17:57:10 -0500

    modif

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,6 +17,6 @@ public class Greeter {
    */
   public final String greet(final String someone) { 
   
-  return String.format("Hello osee tafo, %s!", someone);
+  return String.format("Hello osee fotso, %s!", someone);
   }
 }
